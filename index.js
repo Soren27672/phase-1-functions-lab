b8d1ea693263f2fda2ce835cedb67d1b258b4891
Maxwell Gottesman <maxgman16@yahoo.com>
2022-10-14 19:07:45 -0400

    commit tree 6bfdef6c3bcb57c2412413ed992176079860f5da

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(blockNum) {
+    return Math.abs(42-blockNum);
+}
+
+function distanceFromHqInFeet(blockNum) {
+    return distanceFromHqInBlocks(blockNum) * 264;
+}
+
+function distanceTravelledInFeet(start,end) {
+    return Math.abs(end-start) * 264;
+}
+
+function calculatesFarePrice(start,end) {
+    let dist = distanceTravelledInFeet(start,end);
+    switch (true) {
+        case dist < 400:
+            return 0;
+        case dist < 2000:
+            return (0.02 * (dist - 400));
+        case dist < 2500:
+            return 25;
+        default:
+            return 'cannot travel that far';
+    }
+}
